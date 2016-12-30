478d7c3da59329d385d2e3256ff68377ef3ce70b
Egemen Can Uze <egemencanuze@gmail.com>
2016-12-30 11:11:32 +0300

    Add Redux boilerplate

--- app/index.js
+++ app/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import Welcome from './components/Welcome';
 
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
 render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Welcome} />
-    </Route>
-  </Router>,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Welcome} />
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
