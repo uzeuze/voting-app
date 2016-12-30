0ce45f5df9839451766daa251540bb7586e45b8c
Egemen Can Uze <egemencanuze@gmail.com>
2016-12-30 23:12:34 +0300

    Add voting & add prestart script

--- app/components/Poll.js
+++ app/components/Poll.js
@@ -10,6 +10,12 @@ import {
 } from '../actions';
 
 class Poll extends Component {
+  constructor() {
+    super();
+    this.state = {
+      error: ''
+    }
+  }
   componentWillMount() {
     this.props.clearPoll();
   }
@@ -17,14 +23,87 @@ class Poll extends Component {
   componentDidMount() {
     this.props.fetchPoll(this.props.params.pollId);
   }
+
+  handleFormSubmit({ option, addOption }) {
+    const answer = {};
+    const { pollId } = this.props.params;
+    let url;
+
+    if (addOption) {
+      answer['option'] = { "text": addOption };
+    } else if (option) {
+      answer['option'] = { "id": option };
+    }
+
+    if (!this.props.authenticated) {
+      url = `http://localhost:3000/api/unauth/polls/${pollId}`;
+    } else {
+      url = `http://localhost:3000/api/polls/${pollId}`;
+    }
+
+    const config = {
+      headers: {
+        'Authorization': localStorage.getItem('token')
+      }
+    };
+
+    axios.post(url, answer, config)
+      .then(response => {
+        if (response.data.error) {
+          this.setState({ error: response.data.error });
+        } else {
+          this.setState({ error: '' });
+          this.props.fetchPoll(pollId);
+          browserHistory.push(`/polls/${pollId}?results=true`);
+        }
+      });
+  }
+
+  renderAddOption() {
+    if (this.props.authenticated) {
+      return (
+        <div>
+          <span>You can add another option: </span>
+          <Field
+            name="addOption"
+            component="input"
+            type="text"
+            placeholder="Enter your choice"
+          />
+          </div>
+      );
+    }
+
+    return null;
+  }
+
   render() {
-    if (!this.props.poll) {
+    const { poll, handleSubmit } = this.props;
+
+    if (!poll) {
       return <div>Loading...</div>;
     }
 
+    const options = poll.options.map((option) => {
+      return (
+        <div key={option.optionId}>
+          <Field name="option" component="input" type="radio" value={`${option.optionId}`} />
+          { `${option.text}  -  ${option.voteCount}` }
+        </div>
+      );
+    });
+
     return (
       <div>
-        {this.props.poll.question}
+        <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+          <div>
+            <h1>{poll.question}</h1>
+            {options}
+            {this.renderAddOption()}
+          </div>
+          {this.state.error && <div className="error">{this.state.error}</div>}
+          <Button className="Poll__button" type="submit">VOTE</Button>
+        </form>
       </div>
     );
   }
@@ -37,4 +116,6 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, { fetchPoll, clearPoll })(Poll);
+export default connect(mapStateToProps, { fetchPoll, clearPoll })(reduxForm({
+  form: 'vote_poll'
+})(Poll));
