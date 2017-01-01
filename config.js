8747886e2b1e8fc5814c3e1a5d6830c17889c282
Egemen Can Uze <egemencanuze@gmail.com>
2017-01-01 21:10:29 +0300

    Add production urls

--- config.js
+++ config.js
@@ -1,2 +1,8 @@
-export const API_URL = 'http://localhost:3000/api';
-export const URL = 'http://localhost:3000';
+let API_URL = 'http://localhost:3000/api';
+let URL = 'http://localhost:3000';
+if (process.env.NODE_ENV === 'production') {
+  API_URL = 'https://polls-uzeuze.herokuapp.com';
+  URL = 'https://polls-uzeuze.herokuapp.com';
+}
+
+export { API_URL, URL };
