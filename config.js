6b941496fcb9d17abd93abcec25de8ef658e2584
Egemen Can Uze <egemencanuze@gmail.com>
2017-01-01 21:40:47 +0300

    change urls

--- config.js
+++ config.js
@@ -1,8 +1,2 @@
-let API_URL = 'http://localhost:3000/api';
-let URL = 'http://localhost:3000';
-if (process.env.NODE_ENV === 'production') {
-  API_URL = 'https://polls-uzeuze.herokuapp.com';
-  URL = 'https://polls-uzeuze.herokuapp.com';
-}
-
-export { API_URL, URL };
+export const API_URL = 'https://polls-uzeuze.herokuapp.com/api';
+export const URL = 'https://polls-uzeuze.herokuapp.com';
